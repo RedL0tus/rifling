51b46ff5f8c905474dfa24055ac64e574bb37d29
Kay Lin <i@v2bv.net>
2019-04-24 16:48:54 -0700

    Fix example
    Signed-off-by: Kay Lin <i@v2bv.net>

--- examples/hyper-simple.rs
+++ examples/hyper-simple.rs
@@ -23,14 +23,23 @@ fn main() {
     pretty_env_logger::init_custom_env("RIFLING_LOG");
     let mut cons = Constructor::new();
     let hook = Hook::new("*", Some(String::from("secret")), |delivery: &Delivery| {
-        if let Some(payload) = &delivery.payload {
-            info!(
-                "Bazinga! Received \"{}\" action!",
-                match delivery.delivery_type {
-                    DeliveryType::GitHub => payload["action"].as_str().unwrap(),
-                    DeliveryType::GitLab => payload["event_name"].as_str().unwrap(),
-                }
-            );
+        #[cfg(feature = "parse")]
+        {
+            if let Some(payload) = &delivery.payload {
+                info!(
+                    "Bazinga! Received \"{}\" action!",
+                    match delivery.delivery_type {
+                        DeliveryType::GitHub => payload["action"].as_str().unwrap(),
+                        DeliveryType::GitLab => payload["event_name"].as_str().unwrap(),
+                    }
+                );
+            }
+        }
+        #[cfg(not(feature = "parse"))]
+        {
+            if let Some(event) = &delivery.event {
+                info!("Received \"{}\" action!", event);
+            }
         }
     });
     let another_hook = Hook::new("push", Some(String::from("secret")), |_: &Delivery| {
